bd0ed9c520c6143f437b97f14a6fb4a0c5ed3a37
retrofox <rdsuarez@gmail.com>
2017-02-09 20:06:17 -0300

    rebase: add tests

--- client/state/data-layer/wpcom-http/test/http.js
+++ client/state/data-layer/wpcom-http/test/http.js
@@ -22,37 +22,37 @@ import {
 
 describe( 'http', () => {
 	describe( 'apiVersion shape', () => {
-		it.only( 'should not accept `1` as a valid version value', () => {
+		it( 'should not accept `1` as a valid version value', () => {
 			expect( apiVersionRegex.test( '1' ) ).to.be.false;
 		} );
 
-		it.only( 'should accept `v1` as a valid version value', () => {
+		it( 'should accept `v1` as a valid version value', () => {
 			expect( apiVersionRegex.test( 'v1' ) ).to.be.true;
 		} );
 
-		it.only( 'should not accept `v1.` as a valid version value', () => {
+		it( 'should not accept `v1.` as a valid version value', () => {
 			expect( apiVersionRegex.test( 'v1.' ) ).to.be.false;
 		} );
 
-		it.only( 'should accept `v1.3` as a valid version value', () => {
+		it( 'should accept `v1.3` as a valid version value', () => {
 			expect( apiVersionRegex.test( 'v1.3' ) ).to.be.true;
 		} );
 	} );
 
 	describe( 'apiNamespace shape', () => {
-		it.only( 'should not accept `wpcom` as a valid version value', () => {
+		it( 'should not accept `wpcom` as a valid version value', () => {
 			expect( apiNamespaceRegex.test( 'wpcom' ) ).to.be.false;
 		} );
 
-		it.only( 'should accept `wpcom/v1` as a valid version value', () => {
+		it( 'should accept `wpcom/v1` as a valid version value', () => {
 			expect( apiNamespaceRegex.test( 'wpcom/v1' ) ).to.be.true;
 		} );
 
-		it.only( 'should not accept `wpcom/` as a valid version value', () => {
+		it( 'should not accept `wpcom/` as a valid version value', () => {
 			expect( apiNamespaceRegex.test( 'wpcom/' ) ).to.be.false;
 		} );
 
-		it.only( 'should not accept `wpcom/1` as a valid version value', () => {
+		it( 'should not accept `wpcom/1` as a valid version value', () => {
 			expect( apiNamespaceRegex.test( 'wpcom/1' ) ).to.be.false;
 		} );
 	} );
@@ -100,6 +100,29 @@ describe( 'http', () => {
 			} );
 		} );
 
+		it( 'should return a valid redux action passing random option into request params', () => {
+			const action = http( {
+				apiVersion: 'v2',
+				foo: 'bar',
+				method: 'GET',
+				path: '/path/to/endpoint',
+			} );
+
+			expect( action ).to.eql( {
+				type: WPCOM_HTTP_REQUEST,
+				body: {},
+				method: 'GET',
+				foo: 'bar',
+				path: '/path/to/endpoint',
+				query: {
+					apiVersion: 'v2',
+				},
+				onSuccess: null,
+				onFailure: null,
+				onProgress: null,
+			} );
+		} );
+
 		it( 'should return a valid redux action passing random parameters into the query', () => {
 			const action = http( {
 				apiVersion: 'v2',
@@ -131,7 +154,7 @@ describe( 'http', () => {
 	} );
 
 	describe( 'bad request action', () => {
-		it( 'should return a bad action when `apiVersion` is not defined', () => {
+		it( 'should return a bad action when `apiVersion` and `apiNamespace` are not defined.', () => {
 			const action = http( {
 				path: '/path/to/endpoint',
 				method: 'GET',
@@ -139,7 +162,8 @@ describe( 'http', () => {
 
 			expect( action ).to.eql( {
 				type: WPCOM_HTTP_BAD_REQUEST,
-				error: 'request parameters object is not valid'
+				error: 'request parameters object is not valid',
+				description: 'One of `apiVersion` or `apiNamespace` params must be defined.',
 			} );
 		} );
 
@@ -151,7 +175,8 @@ describe( 'http', () => {
 
 			expect( action ).to.eql( {
 				type: WPCOM_HTTP_BAD_REQUEST,
-				error: 'request parameters object is not valid'
+				error: 'request parameters object is not valid',
+				description: '\'path: undefined\' param is invalid.',
 			} );
 		} );
 
@@ -164,7 +189,8 @@ describe( 'http', () => {
 
 			expect( action ).to.eql( {
 				type: WPCOM_HTTP_BAD_REQUEST,
-				error: 'request parameters object is not valid'
+				error: 'request parameters object is not valid',
+				description: '\'path: true\' param is invalid.',
 			} );
 		} );
 
@@ -176,7 +202,8 @@ describe( 'http', () => {
 
 			expect( action ).to.eql( {
 				type: WPCOM_HTTP_BAD_REQUEST,
-				error: 'request parameters object is not valid'
+				error: 'request parameters object is not valid',
+				description: '\'method: undefined\' param is invalid.',
 			} );
 		} );
 
@@ -189,33 +216,37 @@ describe( 'http', () => {
 
 			expect( action ).to.eql( {
 				type: WPCOM_HTTP_BAD_REQUEST,
-				error: 'request parameters object is not valid'
+				error: 'request parameters object is not valid',
+				description: '\'method: PUT\' param is invalid.',
 			} );
 		} );
 
 		it( 'should return a bad action when apiVersion is invalid', () => {
 			const action = http( {
 				apiVersion: 'version1',
-				method: 'PUT',
+				method: 'GET',
 				path: '/path/to/endpoint',
 			} );
 
 			expect( action ).to.eql( {
 				type: WPCOM_HTTP_BAD_REQUEST,
-				error: 'request parameters object is not valid'
+				error: 'request parameters object is not valid',
+				description: '\'apiVersion: version1\' param is invalid.',
 			} );
 		} );
 
 		it( 'should return a bad action when apiNamespace is invalid', () => {
 			const action = http( {
 				apiNamespace: 'v1',
-				method: 'PUT',
+				method: 'GET',
 				path: '/path/to/endpoint',
+
 			} );
 
 			expect( action ).to.eql( {
 				type: WPCOM_HTTP_BAD_REQUEST,
-				error: 'request parameters object is not valid'
+				error: 'request parameters object is not valid',
+				description: '\'apiNamespace: v1\' param is invalid.',
 			} );
 		} );
 
@@ -223,13 +254,14 @@ describe( 'http', () => {
 			const action = http( {
 				apiVersion: 'v1',
 				apiNamespace: 'wpcom/v2',
-				method: 'PUT',
+				method: 'GET',
 				path: '/path/to/endpoint',
 			} );
 
 			expect( action ).to.eql( {
 				type: WPCOM_HTTP_BAD_REQUEST,
-				error: 'request parameters object is not valid'
+				error: 'request parameters object is not valid',
+				description: '`apiVersion` or `apiNamespace` params cannot be defined simultaneously.'
 			} );
 		} );
 	} );
