912e09677180700a9907b1681af23284ddac352f
retrofox <rdsuarez@gmail.com>
2017-02-08 23:15:49 -0300

    data-layer:wpcom-http: checking request parameters

--- client/state/data-layer/wpcom-http/actions.js
+++ client/state/data-layer/wpcom-http/actions.js
@@ -1,7 +1,24 @@
 /**
  * Internal dependencies
  */
-import { WPCOM_HTTP_REQUEST } from 'state/action-types';
+import {
+	WPCOM_HTTP_REQUEST,
+	WPCOM_HTTP_BAD_REQUEST,
+} from 'state/action-types';
+import warn from 'lib/warn';
+
+export const apiVersionRegex = new RegExp( /^v(?:(\d+)\.)?(\d+)$/ );
+export const apiNamespaceRegex = new RegExp( /^(?:([a-z]+)\/)?v(\d+)$/ );
+
+const badRequest = description => {
+	warn( `wpcom-http error: ${ description }` );
+
+	return {
+		type: WPCOM_HTTP_BAD_REQUEST,
+		error: 'request parameters object is not valid',
+		description
+	};
+};
 
 /**
  * Returns a valid WordPress.com API HTTP Request action object
@@ -18,10 +35,10 @@ import { WPCOM_HTTP_REQUEST } from 'state/action-types';
  * @param {Object} [onProgress] Redux action to call on progress events from an upload
  * @param {Object} [options] extra options to send to the middleware, e.g. retry policy or offline policy
  * @param {Object} [action] default action to call on HTTP events
- * @returns {Object} Redux action describing WordPress.com API HTTP request
+ * @return {Object} Redux action describing WordPress.com API HTTP request
  */
 export const http = ( {
-	apiVersion = 'v1',
+	apiVersion,
 	apiNamespace,
 	body = {},
 	method,
@@ -32,15 +49,66 @@ export const http = ( {
 	onFailure,
 	onProgress,
 	...options,
-}, action = null ) => ( {
-	type: WPCOM_HTTP_REQUEST,
-	body,
-	method,
-	path,
-	query: { ...query, apiVersion, apiNamespace },
-	formData,
-	onSuccess: onSuccess || action,
-	onFailure: onFailure || action,
-	onProgress: onProgress || action,
-	options,
-} );
+}, action = null ) => {
+	const actionProperties = {
+		body,
+		onSuccess: onSuccess || action,
+		onFailure: onFailure || action,
+		onProgress: onProgress || action,
+		query: { ...query },
+	};
+
+	// * check parameters *
+
+	// `path` is not optional and it is a string
+	// `method` is not optional and its value can take either 'GET' or 'POST`
+	if ( ! path || typeof path !== 'string' ) {
+		return badRequest( `'path: ${ path }' param is invalid.` );
+	}
+
+	if ( ! method || ( method !== 'GET' && method !== 'POST' ) ) {
+		return badRequest( `'method: ${ method }' param is invalid.` );
+	}
+
+	// - One of `apiVersion` of `apiNamespace` should be defined
+	if ( typeof apiVersion === 'undefined' && typeof apiNamespace === 'undefined' ) {
+		return badRequest( 'One of `apiVersion` or `apiNamespace` params must be defined.' );
+	}
+
+	// - `apiVersion` and `apiNamespace` cannot be defined simultaneously
+	if ( typeof apiVersion !== 'undefined' && typeof apiNamespace !== 'undefined' ) {
+		return badRequest( '`apiVersion` or `apiNamespace` params cannot be defined simultaneously.' );
+	}
+
+	// - `apiVersion` must have the `v1, v1.1, v2, v2.1, ...` shape
+	if ( typeof apiVersion !== 'undefined' ) {
+		if ( apiVersionRegex.test( apiVersion ) ) {
+			actionProperties.query.apiVersion = apiVersion;
+		} else {
+			return badRequest( `'apiVersion: ${ apiVersion }' param is invalid.` );
+		}
+	}
+
+	// - `apiNamesapce` must have the `wp/v2, wpcom/v2, ...` shape
+	if ( typeof apiNamespace !== 'undefined' ) {
+		if ( /^[a-z]+\/v\d+/.test( apiNamespace ) ) {
+			actionProperties.query.apiNamespace = apiNamespace;
+		} else {
+			return badRequest( `'apiNamespace: ${ apiNamespace }' param is invalid.` );
+		}
+	}
+
+	// optional parameters
+	if ( typeof formData !== 'undefined' ) {
+		actionProperties.formData = [];
+	}
+
+	return {
+		...actionProperties,
+		...options,
+		type: WPCOM_HTTP_REQUEST,
+		path,
+		method,
+		body,
+	};
+};
