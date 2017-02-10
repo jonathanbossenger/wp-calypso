f470d498739cf69391d4e649e4200df6e8d78a97
Steve Seear <steve.seear@gmail.com>
2017-02-10 18:04:39 +0000

    AT: Add new selectors for eligibility warnings/holds
    Will be used by some more specific selectors to determine
    whether to display eligibility card for theme uploads.

---
+++ client/state/selectors/get-automated-transfer-eligibility-holds.js
@@ -0,0 +1,23 @@
+/**
+ * External dependencies
+ */
+import { get } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import { getEligibility } from 'state/automated-transfer/selectors';
+
+/**
+ * Returns any blocking reasons why a site is not eligible
+ * for automated transfer.
+ *
+ * @param {Object} state global app state
+ * @param {number} siteId requested site for transfer info
+ * @returns {Array} Any holds that exist
+ */
+export default function getAutomatedTransferEligiblityHolds( state, siteId ) {
+	const eligibilityData = getEligibility( state, siteId );
+//	console.log( eligibilityData );
+	return get( eligibilityData, 'eligibilityHolds', [] );
+}
